c3150e8361227cd57bf2d73ba78e07ece2cdb423
Prabin <prb977@gmail.com>
2023-05-15 22:57:43 -0400

    large template based inference

---
+++ coor-prediction/slurm/run_llama2.bash
@@ -0,0 +1,10 @@
+MODEL='llama'
+
+model_size='1'
+for p_type in 0 1
+do
+    for p_length in 'zero-shot' '3-shot'
+    do
+        sbatch  --job-name=$MODEL.$p_type.$p_length.$model_size template_large.slurm $MODEL $model_size $p_type $p_length;
+    done;
+done;
