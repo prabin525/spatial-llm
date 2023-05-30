c93ad629c78f13410b387b81cff1db043e712e5f
Prabin <prb977@gmail.com>
2023-05-30 19:06:58 +0545

    Create cities list for geospatial awareness

---
+++ spatial-awareness/create_cities_list.py
@@ -0,0 +1,41 @@
+import json
+import geopy.distance
+from geocoding.geocoding import GeoCoding
+
+in_file_loc = 'places_with_lat_lng_within_continent.txt'
+geocoder = GeoCoding()
+places = []
+with open(in_file_loc) as f:
+    for each in f.readlines():
+        splitted = each.split("\t")
+        lat, lng, _, _, _ = geocoder.get_lat_lng(splitted[0])
+        places.append({
+            'name': splitted[0],
+            'lat': lat,
+            'lng': lng
+        })
+for each in places:
+    places2 = places.copy()
+    places2.remove(each)
+    for i, c in enumerate(places2):
+        if i == 0:
+            dis = geopy.distance.distance(
+                [each['lat'], each['lng']], [c['lat'], c['lng']]
+            ).km
+            near_city = c['name']
+            near_dis = dis
+            far_city = c['name']
+            far_dis = dis
+        else:
+            dis = geopy.distance.distance(
+                [each['lat'], each['lng']], [c['lat'], c['lng']]
+            ).km
+            if dis < near_dis:
+                near_dis = dis
+                near_city = c['name']
+            if dis > far_dis:
+                far_dis = dis
+                far_city = c['name']
+    each['near_city'] = near_city
+    each['far_city'] = far_city
+json.dump(places, open('cities.json', 'w+'))
