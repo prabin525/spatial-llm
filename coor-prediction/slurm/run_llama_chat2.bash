c758f7c269155f90610951bf1b5ac5180e2a50d1
Prabin <prb977@gmail.com>
2024-01-11 19:14:02 -0500

    Update slurm to large GPU

--- coor-prediction/slurm/run_llama_chat2.bash
+++ coor-prediction/slurm/run_llama_chat2.bash
@@ -5,6 +5,6 @@ for p_type in 0 1
 do
     for p_length in 'zero-shot'
     do
-        sbatch  --job-name=$MODEL.$p_type.$p_length.$model_size template_small.slurm $MODEL $model_size $p_type $p_length;
+        sbatch  --job-name=$MODEL.$p_type.$p_length.$model_size template_large.slurm $MODEL $model_size $p_type $p_length;
     done;
 done;
