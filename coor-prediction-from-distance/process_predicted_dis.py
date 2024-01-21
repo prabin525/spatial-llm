f9d78427b855c31fef450c2c76a97f6b288fe15d
Prabin <prb977@gmail.com>
2024-01-21 17:14:44 -0500

    pred coor fromdis llama2 done

--- coor-prediction-from-distance/process_predicted_dis.py
+++ coor-prediction-from-distance/process_predicted_dis.py
@@ -1,18 +1,65 @@
 import json
+import argparse
 import pandas as pd
 
-cities = json.load(open('outputs/gen_dis-3-shot.json'))
 
-for each in cities:
-    o = each['output'][0].replace(each['prompt'], '').rstrip('.')
-    try:
-        o = float(o)
-    except ValueError:
-        o = o.replace(',', '')
-        o = float(o)
-    del each['output']
-    del each['prompt']
-    each['predicted_dis'] = o
+def process_llama():
+    cities = json.load(open('outputs/gen_dis-3-shot.json'))
 
-df = pd.DataFrame.from_dict(cities)
-df.to_pickle('cities.pkl')
+    for each in cities:
+        o = each['output'][0].replace(each['prompt'], '').rstrip('.')
+        try:
+            o = float(o)
+        except ValueError:
+            o = o.replace(',', '')
+            o = float(o)
+        del each['output']
+        del each['prompt']
+        each['predicted_dis'] = o
+
+    df = pd.DataFrame.from_dict(cities)
+    df.to_pickle('cities.pkl')
+
+
+def process_llama2():
+    cities = json.load(open('outputs/gen_dis_llama2-3-shot.json'))
+    df = pd.read_pickle('../spatial-awareness/count_df_llama2.pkl')
+
+    for each in cities:
+        o = each['output'][0].replace(each['prompt'], '').rstrip('.')
+        try:
+            o = float(o)
+        except ValueError:
+            o = o.replace(',', '')
+            o = float(o)
+        del each['output']
+        del each['prompt']
+        each['predicted_dis'] = o
+
+    df_temp = pd.DataFrame.from_dict(cities)
+    df['predicted_dis'] = df_temp['predicted_dis']
+    df.to_pickle('cities_llama2.pkl')
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description='Process predicted distance from LLAMA'
+    )
+    parser.add_argument(
+        '--model',
+        dest='model',
+        choices=[
+            'llama',
+            'llama2',
+        ],
+        default='llama2'
+    )
+    args = parser.parse_args()
+    print(args)
+
+    if args.model == 'llama':
+        process_llama()
+    elif args.model == 'llama2':
+        process_llama2()
+    else:
+        pass
