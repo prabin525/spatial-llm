973fa1988f4b7fe9e07c3950dfe300b458ec089c
Prabin <prb977@gmail.com>
2024-01-28 15:23:06 -0500

    Analysis results for opt

--- coor-prediction-from-distance/process_predicted_dis.py
+++ coor-prediction-from-distance/process_predicted_dis.py
@@ -41,6 +41,29 @@ def process_llama2():
     df.to_pickle('cities_llama2.pkl')
 
 
+def process_opt():
+    cities = json.load(open('outputs/gen_dis_opt-3-shot.json'))
+    df = pd.read_pickle('../spatial-awareness/count_df_opt.pkl')
+
+    for each in cities:
+        o = each['output'][0].replace(each['prompt'], '').rstrip('.')
+        try:
+            o = float(o)
+        except ValueError:
+            try:
+                o = o.replace(',', '')
+                o = float(o)
+            except ValueError:
+                o = None
+        del each['output']
+        del each['prompt']
+        each['predicted_dis'] = o
+
+    df_temp = pd.DataFrame.from_dict(cities)
+    df['predicted_dis'] = df_temp['predicted_dis']
+    df.to_pickle('cities_opt.pkl')
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         description='Process predicted distance from LLAMA'
@@ -51,8 +74,9 @@ if __name__ == '__main__':
         choices=[
             'llama',
             'llama2',
+            'opt',
         ],
-        default='llama2'
+        default='opt'
     )
     args = parser.parse_args()
     print(args)
@@ -61,5 +85,7 @@ if __name__ == '__main__':
         process_llama()
     elif args.model == 'llama2':
         process_llama2()
+    elif args.model == 'opt':
+        process_opt()
     else:
         pass
